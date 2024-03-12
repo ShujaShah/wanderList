8955df559bb45a86403161e6383d263b6a3bad44
shujashah <shuja.ashraf11@gmail.com>
2024-03-12 11:38:14 +0530

    added sample list and styling

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,14 +1,22 @@
-/* 
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: true },
 ];
-*/
 
 import React from 'react';
+import Item from './Item';
 
 const PackingList = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul className="list">
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PackingList;
