8f833168689fd363ad246aa372e6ca09ff4e802c
shujashah <shuja.ashraf11@gmail.com>
2024-03-15 00:08:43 +0530

    added the strike through on checked

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Item from './Item';
 
-const PackingList = ({ items, onDeleteItem }) => {
+const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
   return (
     <div className="list">
       <ul className="list">
         {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
     </div>
