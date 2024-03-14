dd19547ee5bc98e3a1b17bdaaf8d373ee4bb40db
shujashah <shuja.ashraf11@gmail.com>
2024-03-14 23:57:02 +0530

    removed the initial items

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,9 +1,3 @@
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: false },
-  { id: 3, description: 'Charger', quantity: 1, packed: true },
-];
-
 import React from 'react';
 import Item from './Item';
 
